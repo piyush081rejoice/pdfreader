5ec1e0067feb6cc72a5000d627c3fe5d26391c45
Piyush <piyush081.rejoice@gmail.com>
2026-05-08 19:40:42 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteTsconfigPaths()],
+  plugins: [react(), viteTsconfigPaths()],
   build: {
     target: "es2015",
   },
